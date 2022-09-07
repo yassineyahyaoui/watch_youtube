020675de91ee529c0b84be02331ec5ec693ceafa
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2022-09-07 18:47:02 +0100

    watch videos dont close the webdriver

--- functions/watch_videos.py
+++ functions/watch_videos.py
@@ -2,10 +2,12 @@ import csv
 import time
 from selenium import webdriver
 
+
+driver = webdriver.Chrome()
+
+
 def watch_videos(video_id, video_duration):
-    driver = webdriver.Chrome()
     print(video_id + " started")
     driver.get("https://www.youtube.com/shorts/" + video_id)
     time.sleep(int(video_duration))
     print(video_id + " finished")
-    driver.quit()
