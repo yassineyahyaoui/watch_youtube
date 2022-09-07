93f7c3e5283488b298306b067a4c715afe6d83fd
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2022-09-07 19:10:21 +0100

    remove the loop

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ def main():
     #get_videos("FOOT BALL", "UCJvgF5uUL22U7i9tNlPvduA", True)
     with open(os.path.join("data", "FOOT BALL", "videos.csv"), "r") as videos_file:
         content = csv.DictReader(videos_file)
-        for i in range(10):
-            for video in content:
-                print(video["Video id"], video["Video duration"][2:-1])
-                watch_videos(video["Video id"], video["Video duration"][2:-1])
+        for video in content:
+            print(video["Video id"], video["Video duration"][2:-1])
+            watch_videos(video["Video id"], video["Video duration"][2:-1])
 
 
 if __name__ == "__main__":
