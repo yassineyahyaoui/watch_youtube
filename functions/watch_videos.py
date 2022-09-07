8e4b876fb2d672fe68837b0747e1a5dce671b221
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2022-09-07 18:36:24 +0100

    watch videos + video duration

--- functions/watch_videos.py
+++ functions/watch_videos.py
@@ -1,6 +1,11 @@
 import csv
+import time
 from selenium import webdriver
 
-def watch_videos(video_id):
+def watch_videos(video_id, video_duration):
     driver = webdriver.Chrome()
+    print(video_id + " started")
     driver.get("https://www.youtube.com/shorts/" + video_id)
+    time.sleep(int(video_duration))
+    print(video_id + " finished")
+    driver.quit()
